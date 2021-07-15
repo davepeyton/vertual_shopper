476be1e0b458dd3202e2d693c9f55f17d91d23d2
davepeyton <davepeytonmusic@gmail.com>
2021-07-15 12:47:50 +0000

    runmigrations

--- products/models.py
+++ products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=240)
+    friendly_name = models.CharField(max_length=240, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -14,8 +14,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=240, null=True, blank=True)
+    name = models.CharField(max_length=240)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
