5b3f973593daaf1dfa693731c18deedd26c06b11
davepeyton <davepeytonmusic@gmail.com>
2021-07-21 09:34:09 +0000

    update html base

--- products/models.py
+++ products/models.py
@@ -18,8 +18,8 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=240, null=True, blank=True)
-    name = models.CharField(max_length=240)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
