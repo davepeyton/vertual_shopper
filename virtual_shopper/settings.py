896e71b04f4f2c22250d2e0064f9d6194e0337c1
davepeyton <davepeytonmusic@gmail.com>
2021-08-03 08:28:47 +0000

    secret key and heroku

--- virtual_shopper/settings.py
+++ virtual_shopper/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f@ei#64*b*bz3u4yie-$()@cd^l(+c9&@6ypx+r0lm(3%_4hr5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['virtual-shopper.herokuapp.com', 'localhost']
 
