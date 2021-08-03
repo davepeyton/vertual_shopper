ca227d78054691b6de3475030a50c931bbb65f8d
davepeyton <davepeytonmusic@gmail.com>
2021-08-03 15:22:27 +0000

    app name change

--- virtual_shopper/asgi.py
+++ virtual_shopper/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vertual_shopper project.
+ASGI config for virtual_shopper project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vertual_shopper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtual_shopper.settings')
 
 application = get_asgi_application()
--- virtual_shopper/wsgi.py
+++ virtual_shopper/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vertual_shopper project.
+WSGI config for virtual_shopper project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vertual_shopper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'virtual_shopper.settings')
 
 application = get_wsgi_application()
