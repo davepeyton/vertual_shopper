9e78c9fdc329ea1d6cd14ed5b375d1ee5071f9f9
davepeyton <davepeytonmusic@gmail.com>
2021-08-03 14:26:50 +0000

    aws pass

--- virtual_shopper/settings.py
+++ virtual_shopper/settings.py
@@ -161,17 +161,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
-
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'virtual-shopper'
     AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = os.environ.get('AKIA3L6ANDF44RKW5P7O')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('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')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
@@ -183,7 +177,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-
+    
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
